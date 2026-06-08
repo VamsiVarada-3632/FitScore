575d200398caf4629dc11b6a7c35b93de6788dfa
VamsiVarada-3632 <vmsi6940@gmail.com>
2026-06-08 09:42:12 +0530

    feat(store): add file and jd text actions to store

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,11 +1,16 @@
 import { create } from 'zustand';
 
-const useStore = create(() => ({
+const useStore = create((set) => ({
   analyses: [],
   currentAnalysis: null,
   uploadedFile: null,
   jdText: '',
   isAnalyzing: false,
+
+  setUploadedFile: (file) => set({ uploadedFile: file }),
+  setJdText: (text) => set({ jdText: text }),
+  setIsAnalyzing: (val) => set({ isAnalyzing: val }),
+  resetUpload: () => set({ uploadedFile: null, jdText: '' }),
 }));
 
 export default useStore;
